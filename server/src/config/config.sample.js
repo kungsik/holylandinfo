fabf717bc7320d7ea54149b698a0e4c2b102e010
Kyoungsik Kim <kungsik@hotmail.com>
2018-04-26 20:50:19 +0300

    minor change

--- server/src/config/config.sample.js
+++ server/src/config/config.sample.js
@@ -1,9 +1,18 @@
 const path = require('path')
+const crypto = require('crypto')
 
+//db정보, 암호키, 구글맵 api, 암호화 메소드
 module.exports = {
   port: process.env.PORT || 8081,
   db: {
-    database: 'mongodb://id:pass@ds125469.mlab.com:25469/gopilgrims'
+    database: 'mongodb://id:pw@ds125469.mlab.com:25469/gopilgrims'
   },
-  secret: 'thisismysecretkey'
+  secret: 'thisismysecretkey',
+  mapApi: '',
+  encryptpass (password) {
+    const encrypted = crypto.createHmac('sha1', this.secret)
+      .update(password)
+      .digest('base64')
+    return encrypted
+  }
 }
